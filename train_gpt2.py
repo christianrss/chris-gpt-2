6d081afc8c3411d456b7c096aba0a741b7fa8bae
Christian Rafael <christianparadix@gmail.com>
2025-11-10 20:04:26 -0300

    float16, gradient scalers, bfloat16, 300ms

--- train_gpt2.py
+++ train_gpt2.py
@@ -243,6 +243,8 @@ for i in range(50):
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
